39c8d01b87e7b5cab308852f22ac4a17a06dbbf1
Miguel <info@cogitocode.org>
2019-10-25 14:56:19 -0400

    Initial Commit

---
+++ CountX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CountX
+//
+//  Created by Miguel Menendez on 10/25/19.
+//  Copyright © 2019 cogitoCODE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
